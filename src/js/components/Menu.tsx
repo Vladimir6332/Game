2cc6619066949ff735e708c8490c10fffec2d631
Vladimir6332 <vladimir6332@gmail.com>
2021-01-08 02:25:42 +0300

    feat: conplete main menu for app

--- src/js/components/Menu.tsx
+++ src/js/components/Menu.tsx
@@ -1,30 +1,59 @@
-import React, { useEffect } from 'react';
+import React, { useState, useEffect } from 'react';
 import clip from '../../assets/sounds/clip_menu_1.mp3';
 
-const Menu: React.FC = () => {
-  useEffect(() => {
-    const audio = new Audio(clip);
-    audio.load();
-    audio.muted = false;
-    audio.autoplay = true;
+const audio = new Audio(clip);
 
-    console.log(audio);
+const Menu: React.FC = () => {
+  const menuItems: { id: number; value: string }[] = [
+    { id: 1, value: 'single player' },
+    { id: 2, value: 'multiplayer' },
+  ];
+  const [mode, setMode] = useState<string>('single player');
+
+  const handleClick = (newMode: string) => {
+    setMode(newMode);
+  };
+  const handleKeyDown = (newMode: string) => {
+    console.log(newMode);
+  };
+
+  useEffect(() => {
+    if (audio.paused) {
+      audio.muted = false;
+      audio.autoplay = true;
+    }
   });
 
   return (
     <div className="menu-wrapper">
       <nav className="menu">
         <ul className="menu__list">
-          <li className="menu__item menu__item_active">
-            <div className="arrow-down">
-              <span />
-              <span />
-              <span />
-            </div>
-            Single player
-          </li>
-          <li className="menu__item">Multiplayer</li>
+          {menuItems.map((element) => {
+            return (
+              <li
+                onClick={() => handleClick(element.value)}
+                onKeyDown={() => handleKeyDown(element.value)}
+                role="presentation"
+                key={element.id}
+                className={`menu__item${
+                  element.value === mode ? ' menu__item_active' : ''
+                }`}
+              >
+                {element.value === mode ? (
+                  <div className="arrow-down">
+                    <span />
+                    <span />
+                    <span />
+                  </div>
+                ) : null}
+                {element.value}
+              </li>
+            );
+          })}
         </ul>
+        <button type="button" className="menu__play">
+          Play
+        </button>
       </nav>
     </div>
   );
