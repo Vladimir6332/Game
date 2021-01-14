17e3ae8716ac0d762de79a72cc2dbe039704844f
Vladimir6332 <vladimir6332@gmail.com>
2021-01-14 14:45:23 +0300

    feat: add garage to routes in App-component

--- src/js/components/App.tsx
+++ src/js/components/App.tsx
@@ -9,6 +9,34 @@ if (!localStorage.getItem('_statistics')) {
   saveStatistics(0, 0, new Date().toLocaleString(), 10, 0);
 }
 
+const statisticsPropertiesTemplate: {
+  kills: number;
+  deaths: number;
+  lastTime: string;
+  hitPercentage: number;
+} = {
+  kills: 50,
+  deaths: 14,
+  lastTime: new Date().toLocaleString(),
+  hitPercentage: 15,
+};
+
+const funcTemplate1 = (
+  str: string,
+  num: number,
+  options: {
+    damage: number;
+    speedGun: number;
+    speedBullet: number;
+    range: number;
+    bulletPacs: number;
+    cost: number;
+  }
+): void => {
+  console.log(str, num, JSON.stringify(options));
+};
+const ALL_POINTS = 30;
+
 const App: React.FC = () => {
   const [isMenuActive, setMenuActive] = useState<boolean>(true);
   console.log(isMenuActive, setMenuActive);
@@ -18,6 +46,16 @@ const App: React.FC = () => {
         <Switch>
           <Route component={Menu} path="/" exact />
           <Route component={Login} path="/login" />
+          <Route
+            render={() => (
+              <Garage
+                statisticsPerson={statisticsPropertiesTemplate}
+                startTrigger={funcTemplate1}
+                ALL_POINTS={ALL_POINTS}
+              />
+            )}
+            path="/garage"
+          />
         </Switch>
       </div>
     </BrowserRouter>
