babe6906abf785af70a13fd050d7866a72b89155
frenki1231 <su-24@tut.by>
2021-01-17 17:09:45 +0300

    feat: add health and bullet limitation

--- src/js/components/Proba.tsx
+++ src/js/components/Proba.tsx
@@ -31,31 +31,63 @@ function onAssetsLoaded() {
     ),
     angleX: 0,
     angleY: 0,
+    recharge: false,
+    timeRecharge: 500,
   };
-  const tankBad = new PIXI.Sprite(
-    PIXI.Texture.from('assets/images/red/tank.png')
-  );
-  const ganBad = new PIXI.Sprite(
-    PIXI.Texture.from('assets/images/red/bigBOOM/bigBOOM.png')
-  );
+  const tankBad = {
+    sprite: new PIXI.Sprite(PIXI.Texture.from('assets/images/red/tank.png')),
+    health: 600,
+    fullHealth: 600,
+    aim: 400,
+    x: app.screen.width,
+    y: app.screen.height,
+    ganBad: new PIXI.Sprite(
+      PIXI.Texture.from('assets/images/red/bigBOOM/bigBOOM.png')
+    ),
+    angleX: 0,
+    angleY: 0,
+    stepBad: 0,
+    checkAss: false,
+    naprv: false,
+    dead: false,
+    recharge: false,
+    timeRecharge: 500,
+    count: 0,
+  };
   const aim = new PIXI.Graphics();
+  const health = new PIXI.Graphics();
+  const healthBad = new PIXI.Graphics();
+  health.lineStyle(4, 0x000000, 1, 0.5, true);
+  healthBad.lineStyle(4, 0x000000, 1, 0.5, true);
   aim.lineStyle(4, 0x00ff00, 0.5, 0.5, true);
-  const xBadStart = app.screen.width;
-  const yBadStart = app.screen.height;
-  tankBad.pivot.y = tankBad.height / 2;
-  tankBad.pivot.x = tankBad.width / 2;
-  tankBad.width = 100;
-  tankBad.height = 50;
-  tankBad.x = xBadStart - tankBad.width / 2;
-  tankBad.y = yBadStart - tankBad.height / 2;
-  tankBad.rotation = Math.PI;
-  ganBad.y = yBadStart - tankBad.height / 2;
-  ganBad.x = xBadStart - tankBad.width / 2;
-  ganBad.pivot.y = ganBad.height / 2;
-  ganBad.pivot.x = ganBad.width * 0.3;
-  ganBad.width *= 0.1;
-  ganBad.height *= 0.1;
-  ganBad.rotation = Math.PI;
+  tankBad.sprite.pivot.y = tankBad.sprite.height / 2;
+  tankBad.sprite.pivot.x = tankBad.sprite.width / 2;
+  tankBad.sprite.width = 100;
+  tankBad.sprite.height = 50;
+  tankBad.sprite.x = tankBad.x - tankBad.sprite.width / 2;
+  tankBad.sprite.y = tankBad.y - tankBad.sprite.height / 2;
+  tankBad.sprite.rotation = Math.PI;
+  tankBad.ganBad.y = tankBad.y - tankBad.sprite.height / 2;
+  tankBad.ganBad.x = tankBad.x - tankBad.sprite.width / 2;
+  tankBad.ganBad.pivot.y = tankBad.ganBad.height / 2;
+  tankBad.ganBad.pivot.x = tankBad.ganBad.width * 0.3;
+  tankBad.ganBad.width *= 0.1;
+  tankBad.ganBad.height *= 0.1;
+  tankBad.ganBad.rotation = Math.PI;
+  healthBad.drawRect(
+    tankBad.sprite.x - tankBad.sprite.width / 2,
+    tankBad.sprite.y - tankBad.sprite.height / 2 - 20,
+    100,
+    4
+  );
+  healthBad.beginFill(0x00ff00, 1);
+  healthBad.drawRect(
+    tankBad.sprite.x - tankBad.sprite.width / 2,
+    tankBad.sprite.y - tankBad.sprite.height / 2 - 20,
+    100,
+    4
+  );
+  healthBad.endFill();
   tank.sprite.y = tank.y;
   tank.sprite.x = tank.x;
   tank.sprite.pivot.y = tank.sprite.height / 2;
@@ -64,18 +96,38 @@ function onAssetsLoaded() {
   tank.sprite.height = 50;
   tank.sprite.y = tank.y + tank.sprite.height / 2;
   tank.sprite.x = tank.x + tank.sprite.width / 2;
-  aim.drawCircle(tank.sprite.x, tank.sprite.y, 400);
+  aim.drawCircle(tank.sprite.x, tank.sprite.y, tank.aim);
   tank.gan.y = tank.y + tank.sprite.height / 2;
   tank.gan.x = tank.x + tank.sprite.width / 2;
   tank.gan.pivot.y = tank.gan.height / 2;
   tank.gan.pivot.x = tank.gan.width * 0.3;
   tank.gan.width *= 0.1;
   tank.gan.height *= 0.1;
+  health.drawRect(
+    tank.sprite.x - tank.sprite.width / 2,
+    tank.sprite.y - tank.sprite.height / 2 - 20,
+    100,
+    4
+  );
+  health.beginFill(0x00ff00, 1);
+  health.drawRect(
+    tank.sprite.x - tank.sprite.width / 2,
+    tank.sprite.y - tank.sprite.height / 2 - 20,
+    100,
+    4
+  );
+  health.endFill();
   app.view.addEventListener('mousemove', (e) => {
     tank.angleX = e.offsetX;
     tank.angleY = e.offsetY;
-    tank.gan.rotation = anglee(tank.x, tank.y, tank.angleX, tank.angleY);
+    tank.gan.rotation = anglee(
+      tank.sprite.x,
+      tank.sprite.y,
+      tank.angleX,
+      tank.angleY
+    );
   });
+
   window.addEventListener('keypress', (e) => {
     if (e.code === 'KeyW') {
       if (tank.y < tank.sprite.height) {
@@ -110,8 +162,29 @@ function onAssetsLoaded() {
     tank.gan.y = tank.y + tank.sprite.height / 2;
     tank.gan.x = tank.x + tank.sprite.width / 2;
     aim.lineStyle(4, 0x00ff00, 0.5, 0.5, true);
-    aim.drawCircle(tank.sprite.x, tank.sprite.y, 400);
-    if (hitTank(tank.sprite, tankBad)) {
+    aim.drawCircle(tank.sprite.x, tank.sprite.y, tank.aim);
+    health.clear();
+    health.lineStyle(4, 0x000000, 1, 0.5, true);
+    health.drawRect(
+      tank.sprite.x - tank.sprite.width / 2,
+      tank.sprite.y - tank.sprite.height / 2 - 20,
+      100,
+      4
+    );
+    if (tank.health / tank.fullHealth < 0.3) {
+      health.beginFill(0xff0000, 1);
+    } else if (tank.health / tank.fullHealth < 0.6) {
+      health.beginFill(0xffff00, 1);
+    } else {
+      health.beginFill(0x00ff00, 1);
+    }
+    health.drawRect(
+      tank.sprite.x - tank.sprite.width / 2,
+      tank.sprite.y - tank.sprite.height / 2 - 20,
+      (100 * tank.health) / tank.fullHealth,
+      4
+    );
+    if (hitTank(tank.sprite, tankBad.sprite)) {
       console.log('Taran');
     }
     if (tank.angleY !== 0 && tank.angleX !== 0) {
@@ -125,26 +198,40 @@ function onAssetsLoaded() {
   });
 
   app.view.addEventListener('click', (e) => {
+    if (tank.recharge) return;
+    tank.recharge = true;
+    setTimeout(() => {
+      tank.recharge = false;
+    }, tank.timeRecharge);
     const mx = e.offsetX;
     const my = e.offsetY;
-    const dy = createNaprv(mx, my, tank.x, tank.y);
-    const dx = Math.sin(anglee(tank.x, tank.y, mx, my) + Math.PI / 2) * 10;
-    let startX = tank.x + (dx * tank.gan.width * 0.7) / 10;
+    const dy = createNaprv(mx, my, tank.sprite.x, tank.sprite.y);
+    const dx =
+      Math.sin(anglee(tank.sprite.x, tank.sprite.y, mx, my) + Math.PI / 2) * 10;
+    let startX = tank.sprite.x + (dx * tank.gan.width * 0.7) / 10;
     let startY =
-      tank.y -
-      Math.cos(anglee(tank.x, tank.y, mx, my) + Math.PI / 2) *
+      tank.sprite.y -
+      Math.cos(anglee(tank.sprite.x, tank.sprite.y, mx, my) + Math.PI / 2) *
         tank.gan.width *
         0.7;
     const r = new PIXI.Graphics();
+    let final = tank.aim - tank.gan.width * 0.7;
     function paint() {
       r.clear();
       r.beginFill(0x000000, 1);
       r.drawCircle(startX, startY, 2);
-      if (hitBill(tankBad, startX, startY)) {
+      if (hitBill(tankBad.sprite, startX, startY)) {
         r.clear();
         reelContainer.removeChild(r);
         console.log('BOOOM!');
-        findAss();
+        tankBad.health -= 100;
+        if (tankBad.health <= 0) {
+          tankBad.dead = true;
+        }
+        if (!tankBad.checkAss) {
+          tankBad.checkAss = true;
+          findAss();
+        }
         return;
       }
       startX += dx;
@@ -159,79 +246,130 @@ function onAssetsLoaded() {
         reelContainer.removeChild(r);
         return;
       }
+      if (final <= 0) {
+        r.clear();
+        reelContainer.removeChild(r);
+        return;
+      }
+      final -= 10;
       setTimeout(paint, 17);
     }
     reelContainer.addChild(r);
     setTimeout(paint, 0);
   });
 
-  let count = 0;
   function computer() {
-    if (tankBad.rotation === 0) {
-      tankBad.x += 5;
-      ganBad.x += 5;
-    } else if (tankBad.rotation === Math.PI) {
-      tankBad.x -= 5;
-      ganBad.x -= 5;
-    } else if (tankBad.rotation === (3 * Math.PI) / 2) {
-      tankBad.y -= 5;
-      ganBad.y -= 5;
+    if (tankBad.checkAss || tankBad.dead) return;
+    if (tankBad.sprite.rotation === 0) {
+      tankBad.sprite.x += 5;
+      tankBad.ganBad.x += 5;
+    } else if (tankBad.sprite.rotation === Math.PI) {
+      tankBad.sprite.x -= 5;
+      tankBad.ganBad.x -= 5;
+    } else if (tankBad.sprite.rotation === (3 * Math.PI) / 2) {
+      tankBad.sprite.y -= 5;
+      tankBad.ganBad.y -= 5;
     } else {
-      tankBad.y += 5;
-      ganBad.y += 5;
+      tankBad.sprite.y += 5;
+      tankBad.ganBad.y += 5;
     }
-    count += 1;
-    if (count === 50) {
-      count = 0;
-      tankBad.rotation += Math.PI / 2;
-      ganBad.rotation += Math.PI / 2;
-      if (tankBad.rotation === Math.PI * 2) {
-        tankBad.rotation = 0;
-        ganBad.rotation = 0;
+    tankBad.count += 1;
+    if (tankBad.count === 50) {
+      tankBad.count = 0;
+      tankBad.sprite.rotation += Math.PI / 2;
+      tankBad.ganBad.rotation += Math.PI / 2;
+      if (tankBad.sprite.rotation === Math.PI * 2) {
+        tankBad.sprite.rotation = 0;
+        tankBad.ganBad.rotation = 0;
       }
     }
-    if (checkLenght(tank, tankBad)) {
+    healthBad.clear();
+    healthBad.lineStyle(4, 0x000000, 1, 0.5, true);
+    healthBad.drawRect(
+      tankBad.sprite.x - tankBad.sprite.width / 2,
+      tankBad.sprite.y - tankBad.sprite.height / 2 - 20,
+      100,
+      4
+    );
+    if (tankBad.health / tankBad.fullHealth < 0.3) {
+      healthBad.beginFill(0xff0000, 1);
+    } else if (tankBad.health / tankBad.fullHealth < 0.6) {
+      healthBad.beginFill(0xffff00, 1);
+    } else {
+      healthBad.beginFill(0x00ff00, 1);
+    }
+    healthBad.drawRect(
+      tankBad.sprite.x - tankBad.sprite.width / 2,
+      tankBad.sprite.y - tankBad.sprite.height / 2 - 20,
+      (100 * tankBad.health) / tankBad.fullHealth,
+      4
+    );
+    if (checkLenght(tank.sprite, tankBad.sprite)) {
+      tankBad.checkAss = true;
       findAss();
       return;
     }
     setTimeout(computer, 50);
   }
-  let stepBad = 0;
-  let naprv = false;
   function findAss() {
-    const dx = tank.x - tankBad.x;
-    const dy = tank.y - tankBad.y;
-    if (stepBad <= 0) {
-      naprv = Math.abs(dx) > Math.abs(dy);
-      stepBad = naprv ? Math.abs(dx) / 2 : Math.abs(dy) / 2;
+    if (tankBad.dead) {
+      reelContainer.removeChild(tankBad.sprite, tankBad.ganBad, healthBad);
+      return;
     }
-    if (naprv) {
+    const dx = tank.sprite.x - tankBad.sprite.x;
+    const dy = tank.sprite.y - tankBad.sprite.y;
+    if (tankBad.stepBad <= 0) {
+      tankBad.naprv = Math.abs(dx) > Math.abs(dy);
+      tankBad.stepBad = tankBad.naprv ? Math.abs(dx) / 2 : Math.abs(dy) / 2;
+    }
+    if (tankBad.naprv) {
       if (dx > 0) {
-        tankBad.x += 7;
-        tankBad.rotation = 0;
-        ganBad.x += 7;
-        ganBad.rotation = 0;
+        tankBad.sprite.x += 7;
+        tankBad.sprite.rotation = 0;
+        tankBad.ganBad.x += 7;
+        tankBad.ganBad.rotation = 0;
       } else {
-        tankBad.x -= 7;
-        tankBad.rotation = Math.PI;
-        ganBad.x -= 7;
-        ganBad.rotation = Math.PI;
+        tankBad.sprite.x -= 7;
+        tankBad.sprite.rotation = Math.PI;
+        tankBad.ganBad.x -= 7;
+        tankBad.ganBad.rotation = Math.PI;
       }
-    } else if (!naprv) {
+    } else if (!tankBad.naprv) {
       if (dy > 0) {
-        tankBad.y += 7;
-        tankBad.rotation = Math.PI / 2;
-        ganBad.y += 7;
-        ganBad.rotation = Math.PI / 2;
+        tankBad.sprite.y += 7;
+        tankBad.sprite.rotation = Math.PI / 2;
+        tankBad.ganBad.y += 7;
+        tankBad.ganBad.rotation = Math.PI / 2;
       } else {
-        tankBad.y -= 7;
-        tankBad.rotation = (Math.PI * 3) / 2;
-        ganBad.y -= 7;
-        ganBad.rotation = (Math.PI * 3) / 2;
+        tankBad.sprite.y -= 7;
+        tankBad.sprite.rotation = (Math.PI * 3) / 2;
+        tankBad.ganBad.y -= 7;
+        tankBad.ganBad.rotation = (Math.PI * 3) / 2;
       }
     }
-    stepBad -= 7;
-    if (!hitTank(tankBad, tank.sprite)) {
+    tankBad.stepBad -= 7;
+    healthBad.clear();
+    healthBad.lineStyle(4, 0x000000, 1, 0.5, true);
+    healthBad.drawRect(
+      tankBad.sprite.x - tankBad.sprite.width / 2,
+      tankBad.sprite.y - tankBad.sprite.height / 2 - 20,
+      100,
+      4
+    );
+    if (tankBad.health / tankBad.fullHealth < 0.3) {
+      healthBad.beginFill(0xff0000, 1);
+    } else if (tankBad.health / tankBad.fullHealth < 0.6) {
+      healthBad.beginFill(0xffff00, 1);
+    } else {
+      healthBad.beginFill(0x00ff00, 1);
+    }
+    healthBad.drawRect(
+      tankBad.sprite.x - tankBad.sprite.width / 2,
+      tankBad.sprite.y - tankBad.sprite.height / 2 - 20,
+      (100 * tankBad.health) / tankBad.fullHealth,
+      4
+    );
+    if (!hitTank(tankBad.sprite, tank.sprite)) {
       console.log('Тоби пизда!');
       setTimeout(findAss, 50);
     } else {
@@ -239,7 +377,15 @@ function onAssetsLoaded() {
     }
   }
   computer();
-  reelContainer.addChild(aim, tank.sprite, tank.gan, tankBad, ganBad);
+  reelContainer.addChild(
+    aim,
+    tank.sprite,
+    tank.gan,
+    tankBad.sprite,
+    tankBad.ganBad,
+    health,
+    healthBad
+  );
   app.stage.addChild(reelContainer);
 }
 
