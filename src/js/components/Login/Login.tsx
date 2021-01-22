daa5d3d171cf0554c90fbd672eba3a8c5dd6b858
Vladimir6332 <vladimir6332@gmail.com>
2021-01-22 13:48:32 +0300

    feat:add return user profile from login

--- src/js/components/Login/Login.tsx
+++ src/js/components/Login/Login.tsx
@@ -5,26 +5,32 @@ import LoginTabs from './LoginTabs';
 import FormLogin from './FormLogin';
 import Button from '../../utils/Button';
 
-const users = [{ userName: 'admin', password: 'admin' }];
-
 const Login: React.FC = () => {
   const [type, setType] = useState<string>('login');
   const [continueDisabled, setContinueDisabled] = useState<boolean>(true);
 
   const history = useHistory();
 
-  const validateLogin = () => {
+  const submitHandler = async () => {
     const userName = (document.getElementById('username') as HTMLInputElement)
       .value;
     const password = (document.getElementById('password') as HTMLInputElement)
       .value;
+    const data = { login: userName, password };
+    const url = `http://localhost:3000/${type}Auth`;
 
-    const validatedUser = users.find(
-      (user) => user.userName === userName && user.password === password
-    );
+    const res = await fetch(url, {
+      method: 'POST',
+      mode: 'cors',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(data),
+    });
 
-    if (validatedUser) {
-      console.log(validatedUser);
+    const userProfile = await res.json();
+    console.log(userProfile);
+    if (userProfile && userProfile.nickName) {
       setContinueDisabled(false);
     } else setContinueDisabled(true);
   };
@@ -40,29 +46,71 @@ const Login: React.FC = () => {
     // handle error if login got failed...
     console.error(error);
   };
-  const responseGoogle = (googleUser: gapi.auth2.GoogleUser): void => {
+
+  const getUserByGoogleAuth = async (
+    url: string,
+    propToken: { token: string }
+  ) => {
+    const res = await fetch(url, {
+      method: 'POST',
+      mode: 'cors',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(propToken),
+    });
+    const userProfile = await res.json();
+    return userProfile;
+  };
+
+  const responseGoogle = async (
+    googleUser: gapi.auth2.GoogleUser
+  ): Promise<any> => {
     const idToken = googleUser.getAuthResponse(true).id_token;
     const googleId = googleUser.getId();
     const name = googleUser.getBasicProfile();
     console.log(idToken, googleId, name);
+    const url = `http://localhost:3000/googleAuth`;
+    const propToken = { token: idToken };
+    const user: { [key: string]: any } = await getUserByGoogleAuth(
+      url,
+      propToken
+    );
+
+    if (user && user.googleID) {
+      setContinueDisabled(false);
+    }
 
     // Make user login in your system
     // login success tracking...
   };
+
   const clientConfig = {
     client_id:
       '666292902237-cd5e1ic284vkroaaunh467jerkra7fn4.apps.googleusercontent.com',
   };
 
+  const getTitle = (activeTab: string): string => {
+    const START_MESSAGE = 'Press Continue to start the game:';
+    const LOGIN_MESSAGE = 'Login for continuing game:';
+    const REGISTER_MESSAGE = 'Register new user:';
+    const RESET_MESSAGE = 'Reset profile of user:';
+    if (!continueDisabled) return START_MESSAGE;
+    if (activeTab === 'login') return LOGIN_MESSAGE;
+    if (activeTab === 'signUp') return REGISTER_MESSAGE;
+    if (activeTab === 'reset') return RESET_MESSAGE;
+    return LOGIN_MESSAGE;
+  };
+
   const signInOptions = { scope: 'profile' };
   return (
     <div className="login">
-      <LoginTabs setType={setType} />
+      <LoginTabs setType={setType} disabled={continueDisabled} />
       <div className="login-form-container">
-        <h2 className="login__title">Login for continuing game:</h2>
+        <h2 className="login__title">{getTitle(type)}</h2>
         <FormLogin
           type={type}
-          onLogin={validateLogin}
+          onLogin={submitHandler}
           disabled={continueDisabled}
         />
       </div>
