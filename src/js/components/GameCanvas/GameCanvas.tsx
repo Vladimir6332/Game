adcff0b103305f5615ff132206e8c87154b5f853
Vladimir6332 <vladimir6332@gmail.com>
2021-01-25 19:47:11 +0300

    feat: add canvas to game-component

--- src/js/components/GameCanvas/GameCanvas.tsx
+++ src/js/components/GameCanvas/GameCanvas.tsx
@@ -1,7 +1,21 @@
 import React from 'react';
+import PixiApp from '../Proba';
 
 const GameCanvas: React.FC = () => {
-  return <div className="game-canvas">Game Canvas</div>;
+  const canvasRef = React.createRef<HTMLDivElement>();
+  const app = PixiApp;
+
+  React.useEffect(() => {
+    canvasRef.current.appendChild(app.view);
+    return () => {
+      app.stop();
+    };
+  });
+  return (
+    <div className="game-canvas">
+      <div ref={canvasRef} />
+    </div>
+  );
 };
 
 export default GameCanvas;
--- src/js/index.tsx
+++ src/js/index.tsx
@@ -4,4 +4,9 @@ import { BrowserRouter } from 'react-router-dom';
 import { render } from 'react-dom';
 import App from './components/App';
 
-render(<App />, document.getElementById('root'));
+render(
+  <BrowserRouter>
+    <App />
+  </BrowserRouter>,
+  document.getElementById('root')
+);
